4aa16f8ce60809c6a3af62d7f472f1e19fe1aab2
Albert1217 <ismoiljonmuxtorov880@gmail.com>
2023-07-04 10:35:56 +0900

    feat: homepage ads section publishing

--- src/app/screens/HomePage/advertisements.tsx
+++ src/app/screens/HomePage/advertisements.tsx
@@ -2,5 +2,18 @@ import { Container } from "@mui/material";
 import React from "react";
 
 export function Advertisements() {
-  return <div className="top_restaurant_frame">Advertisements</div>;
+  return (
+    <div className="ads_restaurant_frame">
+      <video
+        className={"ads_video"}
+        autoPlay={true}
+        loop
+        muted
+        playsInline
+        data-video-media=""
+      >
+        <source src="./video/video.mp4" type="video/mp4" />
+      </video>
+    </div>
+  );
 }
