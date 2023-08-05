0810847334e037ee9e0240495aa478683b745a0d
Albert1217 <ismoiljonmuxtorov880@gmail.com>
2023-08-06 02:04:45 +0900

    feat: practice useEffect hook with 3 lifecyle method features

--- src/app/screens/HomePage/index.tsx
+++ src/app/screens/HomePage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container } from "@mui/material";
 import { Statistics } from "./statistics";
 import { TopRestaurants } from "./topRestaurants";
@@ -11,6 +11,13 @@ import "../../../css/home.css";
 import { Events } from "./events";
 
 export function HomePage() {
+  useEffect(() => {
+    console.log("componentDidMount => Data fetch");
+    return () => {
+      console.log("componentWillUnmount process");
+    };
+  }, []);
+
   return (
     <div className="homepage">
       <Statistics />
